947f3703dd693d89261a92a601e9ecc591882d9f
dext01 <130887490+dext01@users.noreply.github.com>
2025-10-15 19:04:25 +0700

    Update work2.c

--- senkov/work2.c
+++ senkov/work2.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <time.h>
+extern char *tzname[];
 
-int main(){
+int main(void){
     time_t now;
     struct tm *sp;
-    (void) time (&now);
-    putenv("TZ=PST8PDT");
+    (void)time(&now);
+
+    putenv("TZ=PST8");  /* строго PST = UTC-8, без перехода на летнее */
+    tzset();
 
     printf("%s", ctime(&now));
     sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n", sp -> tm_mon+1, sp -> tm_mday, sp -> tm_year, sp -> tm_hour, sp -> tm_min, tzname[sp -> tm_isdst]);
+    printf("%d/%d/%d %02d:%02d %s\n",
+           sp->tm_mon + 1, sp->tm_mday, sp->tm_year + 1900,
+           sp->tm_hour, sp->tm_min,
+           tzname[sp->tm_isdst]);
+    return 0;
 }
