5327928238cf50fbc01145b14f4e4383cfc54a14
Andrei Chervov <a.chervov@g.nsu.ru>
2025-10-15 14:57:55 +0700

    S1/+1,2,3

---
+++ S1/3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wordexp.h>
+
+void open_test()
+{
+    char fullpath[200];
+    strcpy(fullpath, getenv("HOME"));
+    strcat(fullpath, "/restr");
+    printf("%s\n", fullpath);
+    FILE *restr_f = fopen(fullpath, "r");
+    if (restr_f == NULL)
+    {
+        perror("Couldn't open file");
+    }
+    else
+    {
+        printf("Successfull open.\n");
+        fclose(restr_f);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("UID: %d, EUID: %d\n", getuid(), geteuid());
+    open_test();
+    setuid(0);
+    printf("UID: %d, EUID: %d\n", getuid(), geteuid());
+    open_test();
+    
+    return 0;
+}
