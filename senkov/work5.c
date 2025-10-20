b9c046357277c6953d6219a1577c982192c52c8e
dext01 <130887490+dext01@users.noreply.github.com>
2025-10-20 23:00:29 +0700

    Update work5.c

--- senkov/work5.c
+++ senkov/work5.c
@@ -4,36 +4,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    off_t off[501];
-    int len[501];
-    char buf[257], ch;
-    int fd, i = 0, n = 0, ask;
+enum { MAX_LINES = 500, BUFSZ = 256 };
 
-    if (argc < 2) { fprintf(stderr,"Usage: %s <file>\n", argv[0]); return 1; }
-    if ((fd = open(argv[1], O_RDONLY)) == -1) { perror(argv[1]); return 1; }
+int main(int argc, char **argv) {
+    off_t off[MAX_LINES];     // смещения начала строк
+    int   len[MAX_LINES];     // длины строк (в байтах)
+    int fd, i = 0, cur = 0, ask;
+    char ch, buf[BUFSZ];
+
+    if (argc < 2) { fprintf(stderr, "Usage: %s <file>\n", argv[0]); return 1; }
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) { perror(argv[1]); return 1; }
 
     off[0] = 0;
     while (read(fd, &ch, 1) == 1) {
         if (ch == '\n') {
-            len[i++] = n + 1;
-            off[i] = lseek(fd, 0, SEEK_CUR);
-            n = 0;
-            if (i == 500) break;
+            if (i < MAX_LINES) {
+                len[i++] = cur + 1;                         // включая '\n'
+                if (i < MAX_LINES) off[i] = lseek(fd, 0L, 1); // начало след. строки
+            }
+            cur = 0;
         } else {
-            n++;
+            ++cur;
         }
     }
-    if (n > 0 && i < 500) len[i++] = n;
+    if (cur > 0 && i < MAX_LINES) len[i++] = cur;           // последняя строка без '\n'
 
-    int max = i;
-    while (printf("Line number : "), scanf("%d", &ask) == 1) {
-        if (ask <= 0) break;
-        if (ask > max) { fprintf(stderr, "Bad Line Number\n"); continue; }
-        if (lseek(fd, off[ask - 1], SEEK_SET) < 0) { perror("lseek"); continue; }
-        int want = len[ask - 1]; if (want > 256) want = 256;
-        ssize_t r = read(fd, buf, want);
-        if (r > 0) write(1, buf, r); else fprintf(stderr, "Bad Line Number\n");
+    for (;;) {
+        printf("Line number : ");
+        if (scanf("%d", &ask) != 1 || ask <= 0) break;
+        if (ask > i) { fprintf(stderr, "Bad Line Number\n"); continue; }
+
+        if (lseek(fd, off[ask - 1], 0) == (off_t)-1) { perror("lseek"); continue; }
+        int left = len[ask - 1];
+        while (left > 0) {
+            int chunk = left < BUFSZ ? left : BUFSZ;
+            ssize_t r = read(fd, buf, chunk);
+            if (r <= 0) { fprintf(stderr, "Bad Line Number\n"); break; }
+            (void)write(1, buf, r);
+            left -= (int)r;
+        }
     }
 
     close(fd);
