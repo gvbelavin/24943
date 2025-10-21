36da3cecf5e09bdf1e7d54bf88fdf0f6a9d3a22a
dext01 <130887490+dext01@users.noreply.github.com>
2025-10-21 12:05:37 +0700

    Add work2.c

--- senkov/work2.c
+++ senkov/work2.c
@@ -9,10 +9,11 @@ int main(void){
     struct tm *sp;
     (void)time(&now);
 
-    putenv("TZ=PST8");  /* строго PST = UTC-8, без перехода на летнее */
+    putenv("TZ=PST8PDT");
     tzset();
 
     printf("%s", ctime(&now));
+    
     sp = localtime(&now);
     printf("%d/%d/%d %02d:%02d %s\n",
            sp->tm_mon + 1, sp->tm_mday, sp->tm_year + 1900,
