abc288b510e7041ba22bd898fc56db93be30201f
dext01 <130887490+dext01@users.noreply.github.com>
2025-10-22 16:53:42 +0700

    Update work2.c

--- senkov/work2.c
+++ senkov/work2.c
@@ -2,22 +2,32 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <time.h>
-extern char *tzname[];
 
 int main(void){
     time_t now;
     struct tm *sp;
+    
+    // Получаем текущее время
     (void)time(&now);
 
+    // Устанавливаем временную зону Калифорнии с принудительным летним временем
     putenv("TZ=PST8PDT");
     tzset();
 
-    printf("%s", ctime(&now));
-    
+    // Преобразуем время
     sp = localtime(&now);
+    
+    // Принудительно устанавливаем летнее время
+    sp->tm_isdst = 1;
+    
+    printf("California Time (PDT): ");
     printf("%d/%d/%d %02d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday, sp->tm_year + 1900,
-           sp->tm_hour, sp->tm_min,
-           tzname[sp->tm_isdst]);
+           sp->tm_mon + 1,      // месяц (1-12)
+           sp->tm_mday,         // день месяца
+           sp->tm_year + 1900,  // год
+           sp->tm_hour,         // часы
+           sp->tm_min,          // минуты
+           tzname[1]);          // принудительно используем летнее время (PDT)
+    
     return 0;
 }
