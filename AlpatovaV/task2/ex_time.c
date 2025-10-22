bc2d3befa5ae0494caa198e42ab29fcbe6773b6f
Veronika Alpatova <v.alpatova1@g.nsu.ru>
2025-10-22 18:43:34 +0700

    add tasks

--- AlpatovaV/task2/ex_time.c
+++ AlpatovaV/task2/ex_time.c
@@ -1,22 +1,32 @@
-#include <sys/types.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-extern char *tzname[];
+#include <sys/types.h>
+#include <time.h>
 
-int main()
-{
+int main(void){
     time_t now;
     struct tm *sp;
-    putenv("TZ=PST8PDT");
-    (void) time( &now );
+    
+    (void)time(&now);
 
-    printf("%s", ctime( &now ) );
+
+    putenv("TZ=PST8");
+    tzset();
+
 
     sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
-    sp->tm_mon + 1, sp->tm_mday,
-    sp->tm_year%100, sp->tm_hour,
-    sp->tm_min, tzname[sp->tm_isdst]);
-    exit(0);
+    
+
+    sp->tm_isdst = 0;
+    
+    printf("California Time (PST): ");
+    printf("%d/%d/%d %02d:%02d %s\n",
+           sp->tm_mon + 1,  
+           sp->tm_mday,        
+           sp->tm_year + 1900, 
+           sp->tm_hour,     
+           sp->tm_min, 
+           tzname[0]);   
+    
+    return 0;
 }
