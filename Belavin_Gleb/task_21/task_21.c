283638dd27d071e7b04d1d07c3110cb1e71b2247
gvbelavin <mcglebich@gmail.com>
2025-11-26 17:39:38 +0700

    new task_21

--- Belavin_Gleb/task_21/task_21.c
+++ Belavin_Gleb/task_21/task_21.c
@@ -6,17 +6,22 @@ volatile sig_atomic_t count = 0;
 
 void sigint_handler(int s) {
     count++;
-    write(1, "\a", 1);
+    write(STDOUT_FILENO, "\a", 1);   // звуковой сигнал
 }
 
 void sigquit_handler(int s) {
-    printf("\nСигналов: %d\n", count);
-    _exit(0);
+    printf("\nСигналов SIGINT: %d\n", count);
+    _exit(0);                        // быстрый выход из обработчика
 }
 
-int main() {
+int main(void) {
     signal(SIGINT, sigint_handler);
     signal(SIGQUIT, sigquit_handler);
-    printf("CTRL-C: beep, CTRL-\\: exit\n");
-    while(1) pause();
+
+    printf("CTRL-C: beep, CTRL-\\\\: exit\n");
+
+    while (1)
+        pause();                    // ждём сигналы
+
+    return 0;
 }
